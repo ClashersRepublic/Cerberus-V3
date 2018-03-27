f570946bc0ad2380dcf6bc0218e74f31c0665e0a
Aidid Haiqal <aididhaiqal@yahoo.com>
2018-03-27 21:15:34 +0800

    remove 5.x solution

--- src/CR.Servers/CR.Servers.CoC.5.X/Packets/Commands/Client/List/BuildingToMove.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CR.Servers.CoC.Packets.Commands.Client.List
-{
-    internal struct BuildingToMove
-    {
-        internal int Id;
-        internal int X;
-        internal int Y;
-    }
-}
--- src/CR.Servers/CR.Servers.CoC.5.X/Packets/Commands/Client/List/UnitToRemove.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CR.Servers.CoC.Packets.Commands.Client.List
-{
-    internal struct UnitToRemove
-    {
-        internal int Type;
-        internal int Id;
-        internal int Count;
-    }
-}
