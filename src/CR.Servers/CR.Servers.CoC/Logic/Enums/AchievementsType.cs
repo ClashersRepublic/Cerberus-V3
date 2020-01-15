69b3050dbc9e3ca47689d3e2d3598c514de04204
DARK_GEMS <37537996+Enjoyop@users.noreply.github.com>
2020-01-15 23:51:39 +0300

    Create AchievementsType.cs

---
+++ src/CR.Servers/CR.Servers.CoC/Logic/Enums/AchievementsType.cs
@@ -0,0 +1,24 @@
+namespace CR.Servers.CoC.Logic.Enums
+{
+  public enum AchievementsType
+  {
+    NPC_STARS = 0;
+    UPGRADE = 1;
+    VICTORY_POINTS = 2;
+    UNIT_UNLOCK = 3;
+    CLEAR_OBSTACLES = 4;
+    DONATE_UNITS = 5;
+    LOOT = 6;
+    DESTROY = 9;
+    WIN_PVP_DEFENSE = 10;
+    WIN_PVP_ATTACK = 11;
+    LEAGUE = 12;
+    WAR_STARS = 13;
+    WAR_LOOT = 14;
+    DONATE_SPELLS = 15;
+    ACCOUNT_BOUND = 16;
+    VERSUS_BATTLE_TROPHIES = 17;
+    GEAR_UP = 18;
+    REPAIR_BUILDING = 19;
+  }
+}
