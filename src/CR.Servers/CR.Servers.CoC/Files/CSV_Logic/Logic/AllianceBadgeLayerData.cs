a8c49cc00da22cd78824f27c369cf4d31c0ceac3
DARK_GEMS <37537996+Enjoyop@users.noreply.github.com>
2020-01-22 13:58:47 +0300

    Update AllianceBadgeLayerData.cs

--- src/CR.Servers/CR.Servers.CoC/Files/CSV_Logic/Logic/AllianceBadgeLayerData.cs
+++ src/CR.Servers/CR.Servers.CoC/Files/CSV_Logic/Logic/AllianceBadgeLayerData.cs
@@ -13,5 +13,6 @@
         public string Type { get; set; }
         public string SWF { get; set; }
         public string ExportName { get; set; }
+        public int RequiredClanLevel { get; set; }
     }
 }
