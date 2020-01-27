be04664620438e57580f75d85810c36bfcea60eb
free jacow gwn <jakovmarkovanovic@gmail.com>
2020-01-27 12:53:03 +0100

    Update

--- src/CR.Servers/CR.Servers.CoC/Program.cs
+++ src/CR.Servers/CR.Servers.CoC/Program.cs
@@ -17,6 +17,7 @@ namespace CR.Servers.CoC
         public static IConfigurationRoot Configuration { get; set; }
 
         public static int Op;
+//new mongodb Clash of Clans 9.256.x
 
         private static void Main()
         {
